8298f03df6a3191002106cc08b078baaa88d30a4
Eric Kersten <ekersten@gmail.com>
2019-05-17 16:24:20 -0300

    setting-django-shell-shell-plus-and-ipython

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -12,9 +12,9 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# INSTALLED_APPS = INSTALLED_APPS + [
-#     'debug_toolbar'
-# ]
+INSTALLED_APPS = INSTALLED_APPS + [
+    'django_extensions'
+]
 
 # MIDDLEWARE = MIDDLEWARE + [
 #     'debug_toolbar.middleware.DebugToolbarMiddleware',
