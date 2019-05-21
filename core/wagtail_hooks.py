aac8cc07797942146bcdcb1c1a5d3ee7b3e77bd7
Eric Kersten <ekersten@gmail.com>
2019-05-21 16:08:35 -0300

    adding-custom-css-and-javascript-to-your-wagtail-admin-area

---
+++ core/wagtail_hooks.py
@@ -0,0 +1,12 @@
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import format_html
+
+from wagtail.core import hooks
+
+@hooks.register('insert_global_admin_css', order=100)
+def global_admin_css():
+    return format_html('<link rel="stylesheet" href="{}">', static('css/custom.css'))
+
+@hooks.register('insert_global_admin_js', order=100)
+def global_admin_js():
+    return format_html('<script src="{}"></script>', static('js/custom.js'))
